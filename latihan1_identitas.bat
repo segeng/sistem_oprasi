755e44a65f926c97c58fdfd6729ad98678a2a032
segeng <136777004+segeng@users.noreply.github.com>
2023-06-16 11:01:17 +0700

    Add files via upload

---
+++ latihan1_identitas.bat
@@ -0,0 +1,8 @@
+@echo off
+title latihan 2 - Menampilkan identitas diri
+echo NIM            : 20121079
+echo NAMA           : M Bagus Ageng Susilo
+echo No Wa          : 081229110702
+echo Alamat         : jalan-jalan asalkan senang
+echo Status         : Bujangan
+pause  
---
+++ latihan2_setting_variabel.bat
@@ -0,0 +1,15 @@
+@echo off
+title Latihan 2 Setting Variabel 
+
+rem =================setting variabel===============
+set nim=20121079
+set nama=M Bagus Ageng Susilo
+set wa=081229110702
+set alamat=Jalan-jalan asalkan senang
+
+rem ================pemanggilan variabel===============
+echo NIM        : %nim%
+echo nama       : %nama%
+echo No Wa      : %wa%
+echo Alamat     : %alamat%
+pause
